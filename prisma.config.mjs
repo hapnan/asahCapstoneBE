1242709c05acf57a598a6c66e83edf1db85fbe4a
TatakAdi <tatakadisubagyo@gmail.com>
2025-11-30 22:54:56 +0800

    fix: schema-prisma from typescript oriented to javascript oriented

--- prisma.config.mjs
+++ prisma.config.mjs
@@ -1,13 +1,13 @@
-import 'dotenv/config';
-import { defineConfig, env } from 'prisma/config';
+import "dotenv/config";
+import { defineConfig, env } from "prisma/config";
 
 export default defineConfig({
-    schema: 'prisma/schema.prisma',
-    migrations: {
-        path: 'prisma/migrations',
-    },
-    datasource: {
-        // Use direct connection for migrations
-        url: env('DIRECT_DATABASE_URL'),
-    },
+  schema: "prisma/schema.prisma",
+  migrations: {
+    path: "prisma/migrations",
+  },
+  datasource: {
+    // Use direct connection for migrations
+    url: env("DIRECT_DATABASE_URL"),
+  },
 });
--- src/plugins/prisma.js
+++ src/plugins/prisma.js
@@ -1,35 +1,36 @@
-import { PrismaClient } from '../generated/prisma/index.js';
-import { withAccelerate } from '@prisma/extension-accelerate';
-import 'dotenv/config';
+import { PrismaClient } from "../generated/prisma/index.js";
+import { withAccelerate } from "@prisma/extension-accelerate";
+import "dotenv/config";
 
 // plugin to instantiate Prisma Client
 const prismaPlugin = {
-    name: 'prisma',
-    register: async function (server) {
-        const prisma = new PrismaClient({
-            // Use Accelerate connection string
-            // Uncomment 👇 for logs
-            log: ['error', 'warn', 'query'],
-        }).$extends(
-            withAccelerate({
-                cache: {
-                    // Enable caching with default TTL of 60 seconds
-                    ttl: 60,
-                },
-            })
-        );
+  name: "prisma",
+  register: async function (server) {
+    const prisma = new PrismaClient({
+      // Use Accelerate connection string
+      accelerateUrl: process.env.DATABASE_URL,
+      // Uncomment 👇 for logs
+      log: ["error", "warn", "query"],
+    }).$extends(
+      withAccelerate({
+        cache: {
+          // Enable caching with default TTL of 60 seconds
+          ttl: 60,
+        },
+      })
+    );
 
-        server.app.prisma = prisma;
+    server.app.prisma = prisma;
 
-        // Close DB connection after the server's connection listeners are stopped
-        // Related issue: https://github.com/hapijs/hapi/issues/2839
-        server.ext({
-            type: 'onPostStop',
-            method: async (server) => {
-                await server.app.prisma.$disconnect();
-            },
-        });
-    },
+    // Close DB connection after the server's connection listeners are stopped
+    // Related issue: https://github.com/hapijs/hapi/issues/2839
+    server.ext({
+      type: "onPostStop",
+      method: async (server) => {
+        await server.app.prisma.$disconnect();
+      },
+    });
+  },
 };
 
 export default prismaPlugin;
